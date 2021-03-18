7f72ab3a8dd8675e28da3524c4b6393e16426b16
rekhaverma <rekha.verma@quovantis.com>
2021-03-18 17:23:28 +0530

    Pass varuable from popup

--- content.js
+++ content.js
@@ -1,42 +1,3 @@
-const psaSelectors = {
-  certification: {
-    selector: "#mainContent > div > table > tbody > tr:nth-child(1) > th",
-    nextSibling: true,
-  },
-  labelType: {
-    selector:
-      "#mainContent > div > table > tbody > tr:nth-child(2) > td > span",
-  },
-  year: {
-    selector: "#mainContent > div > table > tbody > tr:nth-child(4) > th",
-    nextSibling: true,
-  },
-  brand: {
-    selector: "#mainContent > div > table > tbody > tr:nth-child(5) > th",
-    nextSibling: true,
-  },
-  sport: {
-    selector: "#mainContent > div > table > tbody > tr:nth-child(6) > th",
-    nextSibling: true,
-  },
-  cardHash: {
-    selector: "#mainContent > div > table > tbody > tr:nth-child(7) > th",
-    nextSibling: true,
-  },
-  player: {
-    selector: "#mainContent > div > table > tbody > tr:nth-child(8) > th",
-    nextSibling: true,
-  },
-  variety_pedigree: {
-    selector: "#mainContent > div > table > tbody > tr:nth-child(9) > th",
-    nextSibling: true,
-  },
-  grade: {
-    selector: "#mainContent > div > table > tbody > tr:nth-child(10) > th",
-    nextSibling: true,
-  },
-};
-
 const executeSelectors = function (config) {
   const refinedObject = {};
   Object.keys(config).map(function (key, index) {
@@ -50,5 +11,7 @@ const executeSelectors = function (config) {
   return refinedObject;
 };
 
-let data = executeSelectors(psaSelectors);
-console.log(data);
+let data = executeSelectors(config);
+chrome.storage.sync.set({ websiteJson: data }, function () {
+  console.log("value set");
+});
--- popup.js
+++ popup.js
@@ -1,11 +1,61 @@
 var tab_title = "";
+let psaSelectors = {
+  certification: {
+    selector: "#mainContent > div > table > tbody > tr:nth-child(1) > th",
+    nextSibling: true,
+  },
+  labelType: {
+    selector:
+      "#mainContent > div > table > tbody > tr:nth-child(2) > td > span",
+  },
+  year: {
+    selector: "#mainContent > div > table > tbody > tr:nth-child(4) > th",
+    nextSibling: true,
+  },
+  brand: {
+    selector: "#mainContent > div > table > tbody > tr:nth-child(5) > th",
+    nextSibling: true,
+  },
+  sport: {
+    selector: "#mainContent > div > table > tbody > tr:nth-child(6) > th",
+    nextSibling: true,
+  },
+  cardHash: {
+    selector: "#mainContent > div > table > tbody > tr:nth-child(7) > th",
+    nextSibling: true,
+  },
+  player: {
+    selector: "#mainContent > div > table > tbody > tr:nth-child(8) > th",
+    nextSibling: true,
+  },
+  variety_pedigree: {
+    selector: "#mainContent > div > table > tbody > tr:nth-child(9) > th",
+    nextSibling: true,
+  },
+  grade: {
+    selector: "#mainContent > div > table > tbody > tr:nth-child(10) > th",
+    nextSibling: true,
+  },
+};
 function display_h1(results) {
   h1 = results;
   document.querySelector("#id1").innerHTML =
     "<p>tab title: " + tab_title + "</p><p>dom h1: " + h1 + "</p>";
 }
-chrome.tabs.query({ active: true }, function (tabs) {
-  var tab = tabs[0];
-  tab_title = tab.title;
-  chrome.tabs.executeScript({ file: "content.js" });
-});
+
+document.getElementById("scan-page-content").onclick = function () {
+  chrome.tabs.executeScript(
+    {
+      code: "var config = " + JSON.stringify(psaSelectors),
+    },
+    function () {
+      chrome.tabs.executeScript({ file: "content.js" });
+    }
+  );
+  chrome.storage.sync.get(["websiteJson"], function (result) {
+    console.log("Value currently is " + JSON.stringify(result));
+    document.getElementById("container").innerHTML = JSON.stringify(
+      result.websiteJson
+    );
+  });
+};
