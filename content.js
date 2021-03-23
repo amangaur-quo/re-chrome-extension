39ee4db407998a8239871ec2924edbc11f83b530
rekhaverma <rekha.verma@quovantis.com>
2021-03-23 18:35:03 +0530

    Added comment

--- content.js
+++ content.js
@@ -1,3 +1,10 @@
+/*
+ *** Content scripts are files that run in the context of web pages.
+ *  By using the standard Document Object Model (DOM), they are able to read details
+ *  of the web pages the browser visits, make changes to them, and pass information to
+ *  their parent extension.
+ */
+
 // Execute DOM selectors for different configuration files
 executeSelectors = function (config) {
   const refinedObject = {};
