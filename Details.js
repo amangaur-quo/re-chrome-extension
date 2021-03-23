3e78efaeca3007c7aa5e11b5e85355ac44d40bb2
Aman Gaur <amangaur@quovantis.com>
2021-03-23 13:32:52 +0530

    trigger function for filling form values from chrome extension storage

--- Details.js
+++ Details.js
@@ -20,11 +20,17 @@ chrome.storage.sync.get(["websiteJson"], function (result) {
     websiteData.certification;
   document.getElementById("labelType").innerText = websiteData.labelType;
   document.getElementById("year").innerText = websiteData.year;
-  document.getElementById("brand").innerText = websiteData.certification;
+  document.getElementById("brand").innerText = websiteData.brand;
   document.getElementById("sport").innerText = websiteData.sport;
   document.getElementById("cardHash").innerText = websiteData.cardHash;
   document.getElementById("player").innerText = websiteData.player;
   document.getElementById("varietyPedigree").innerText =
     websiteData.variety_pedigree;
   document.getElementById("cardGrade").innerText = websiteData.grade;
+});
+
+document.getElementById("FillValues").addEventListener("click", () => {
+  chrome.tabs.executeScript({
+    file: "fill.js",
+  });
 });
---
+++ fill.js
@@ -0,0 +1,20 @@
+elementIdsArray = [
+  { title: "grade", id: "mat-input-4" },
+  { title: "player", id: "mat-input-5" },
+  { title: "sport", id: "mat-select-value-5" },
+  { title: "brand", id: "mat-input-6" },
+  { title: "year", id: "mat-input-7" },
+  { title: "cardHash", id: "mat-input-8" },
+];
+
+(function fillForm() {
+  chrome.storage.sync.get(["websiteJson"], function (result) {
+    console.log("Value currently is " + JSON.stringify(result));
+    const websiteData = result.websiteJson;
+    console.log("websiteDate", websiteData);
+    elementIdsArray.forEach((element) => {
+      document.getElementById(element.id).value = websiteData[element.title];
+      document.getElementById(element.id).dispatchEvent(new Event("input"));
+    });
+  });
+})();
